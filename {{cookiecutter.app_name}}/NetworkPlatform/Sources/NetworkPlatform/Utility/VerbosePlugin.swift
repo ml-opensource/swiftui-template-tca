02cb78652cb65f61fa958cd028a94a9174fec7e2
Mohammed Rokon Uddin <rokon.uddin@monstar-lab.com>
2023-02-07 10:17:06 +0600

    feat: add Utilities package to the template

--- {{cookiecutter.app_name}}/NetworkPlatform/Sources/NetworkPlatform/Utility/VerbosePlugin.swift
+++ {{cookiecutter.app_name}}/NetworkPlatform/Sources/NetworkPlatform/Utility/VerbosePlugin.swift
@@ -6,43 +6,41 @@
 //  Copyright © {% now 'utc', '%Y' %} {{cookiecutter.company_name}}. All rights reserved.
 //
 
-import Combine
 import Foundation
+import Combine
 import Moya
+import Utilities
 
 struct VerbosePlugin: PluginType {
     let verbose: Bool
-
+    
     func prepare(_ request: URLRequest, target _: TargetType) -> URLRequest {
-        #if DEBUG
-            if let body = request.httpBody,
-               let str = String(data: body, encoding: .utf8)
-            {
-                if verbose {
-                    print("request to send: \(str))")
-                }
+#if DEBUG
+        if let body = request.httpBody,
+           let string = String(data: body, encoding: .utf8) {
+            if verbose {
+                Logger.info("Request Body: \(string))")
             }
-        #endif
+        }
+#endif
         return request
     }
-
+    
     func didReceive(_ result: Result<Response, MoyaError>, target _: TargetType) {
-        #if DEBUG
-            switch result {
-            case let .success(body):
-                if verbose {
-                    print("Response:")
-                    if let json = try? JSONSerialization.jsonObject(with: body.data, options: .mutableContainers)
-                    {
-                        print(json)
-                    } else {
-                        let response = String(data: body.data, encoding: .utf8)!
-                        print(response)
-                    }
+#if DEBUG
+        switch result {
+        case let .success(body):
+            if verbose {
+                if let json = try? JSONSerialization.jsonObject(with: body.data, options: .mutableContainers) {
+                    Logger.info("Response: \(json))")
+                } else {
+                    let response = String(data: body.data, encoding: .utf8)!
+                    Logger.info("Response: \(response))")
                 }
-            case .failure:
-                break
             }
-        #endif
+        case .failure:
+            break
+        }
+#endif
     }
 }
